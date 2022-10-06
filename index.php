3bb729aeec71ea483a050b5d321441c4be8487e8
amalinazarr <amalinazarr@gmail.com>
2022-10-06 17:14:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>Log In</title>
+</head>
+<body>
+    <div class="container">
+        <form action=""method="POST" class="login-email">
+            <p style="font-size:2rem;font-weihgt:850;">LOG IN</P>
+
+            <div class="input-group"><input type="text" placeholder="User Name" name="username"></div> 
+            <div class="input-group"><input type="password" placeholder="Password" name="password"></div>
+            <div class="input-group"><button name = "submit"
+            class="btn">Log In</button></div>
+            <p class="login-register-text">Don't Have an Account ? <a href="register.php">Register</a></p>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Register</title>
+</head>
+<body>
+    <div class="container">
+        <form action=""method="POST" class="login-email">
+            <p style="font-size:2rem;font-weihgt:850;">REGISTER</P>
+
+            <div class="input-group"><input type="text" placeholder="User Name" name="username"></div> 
+            <div class="input-group"><input type="password" placeholder="Password" name="password"></div>
+            <div class="input-group"><input type="password" placeholder="Confirm Password" name="cpassword"></div> 
+            <div class="input-group"><button name = "submit"
+            class="btn">Register</button></div>
+            <p class="login-register-text">Have an Account ? <a href="index.php">Log In</a></p>
+        </form>
+    </div>
+</body>
+
+</html>
